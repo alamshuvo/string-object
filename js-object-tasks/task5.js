5ad2d343589da258c9ecb45e7e8ef115191aac4f
iftakhar <iftakharalamshuvo11@gmail.com>
2024-01-31 13:21:12 +0600

    task 5

--- js-object-tasks/task5.js
+++ js-object-tasks/task5.js
@@ -17,3 +17,18 @@
 //     key: city | type:  string
 //     key: isStudent | type:  boolean
 
+let myObject = {
+    name: 'John Doe',
+    age: 25,
+    city: 'Example City',
+    isStudent: true
+};
+for (let key in myObject) {
+    let values=myObject[key];
+    let types=typeof(values);
+    let convertedValue= `key : ${key} | type:  ${types}`;
+    console.log(convertedValue);
+}
+
+// object er jonno sadaronoto for in loop use kora hoi .for in loop er maddhome object er key and value access kora jai and then oi access kora key and value er moddhe type check kora jai /sob sese literal templeate er maddhome output er moto koro add kora jai .
+ 
